165d609a99d4e5e716c75f1c726ff97ebbea904d
Nattapong Pradubsook <pradabsook@gmail.com>
2016-03-15 21:09:31 +0700

    Update db_gmap.sql

--- db_gmap.sql
+++ db_gmap.sql
@@ -1,49 +1,35 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: 
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+Source Server         : localhost
+Source Server Version : 50617
+Source Host           : localhost:3306
+Source Database       : db_gmap
 
+Target Server Type    : MYSQL
+Target Server Version : 50617
+File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+Date: 2016-03-15 21:09:08
+*/
 
---
--- Database: `db_gmap`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
-
---
--- โครงสร้างตาราง `tbl_location`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_location` (
+-- ----------------------------
+-- Table structure for `tbl_location`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_location`;
+CREATE TABLE `tbl_location` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `lat` varchar(50) NOT NULL,
   `lng` varchar(50) NOT NULL,
   `location_name` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
---
--- dump ตาราง `tbl_location`
---
-
-INSERT INTO `tbl_location` (`id`, `lat`, `lng`, `location_name`) VALUES
-(1, '', '', ''),
-(2, '', '', ''),
-(3, '', '', ''),
-(4, '16.08276411471161', '103.677978515625', 'ร้อยเอ็ด');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of tbl_location
+-- ----------------------------
+INSERT INTO `tbl_location` VALUES ('4', '16.08276411471161', '103.677978515625', 'ร้อยเอ็ด');
+INSERT INTO `tbl_location` VALUES ('5', '16.05637148556169', '103.1671142578125', 'มหาสารคาม');
+INSERT INTO `tbl_location` VALUES ('6', '15.056210365453422', '103.106689453125', 'บุรีรัมย์');
